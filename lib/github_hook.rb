57df1f9f89149765136d4dc10d6374d20b7f00e6
Sue White <sue@mailworks.org>
2013-11-21 15:27:17 -0800

    second commit

--- lib/github_hook.rb
+++ lib/github_hook.rb
@@ -5,7 +5,7 @@ require 'yaml'
 class GithubHook < Sinatra::Base
   def self.parse_git
     # Parse hash and date from the git log command.
-    sha1, date = `git log HEAD~1..HEAD --pretty=format:%h^%ci`.strip.split('^')
+    sha1, date = `git log HEAD~1..HEAD --pretty=format:"%h^%ci"`.strip.split('^')
     set :commit_hash, sha1
     set :commit_date, Time.parse(date)
   end
